86cf570841d2479f1870dca9395fde9c12dd5f95
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-12 07:27:14 +0300

    Button listeners

--- KimaigaAndroid/app/src/main/java/com/example/kimaiga/kimaigaandroid/MyActivity.java
+++ KimaigaAndroid/app/src/main/java/com/example/kimaiga/kimaigaandroid/MyActivity.java
@@ -4,13 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends AppCompatActivity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                button.setText("I am clicked");
+            }
+
+        });
+
     }
 
     @Override
